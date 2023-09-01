b1ade9c24e0da4ba4e041275016deda99b5b7311
E1Bos <22bosluc@sssrome.it>
2023-09-01 14:55:58 +0200

    changed version number to 1.5.9

--- data/instalocker.py
+++ data/instalocker.py
@@ -77,7 +77,7 @@ class InstalockerGUIMain(customtkinter.CTk):
         super().__init__()
 
         # Version
-        CURRENT_VERSION = "v1.5.8"
+        CURRENT_VERSION = "v1.5.9"
 
         # Locking Variables
         self.coords = {
