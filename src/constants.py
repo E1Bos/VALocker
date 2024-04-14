d485bc963be4a8795b3aa2019ed70b8becf994c0
E1Bos <lboscolo68@gmail.com>
2024-04-14 13:29:24 +0200

    Add constants RELEASE_URL and API_RELEASE_URL to constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,4 @@
 DOWNLOAD_URL = "https://raw.githubusercontent.com/E1Bos/VALocker/valocker-v2"
+RELEASE_URL = "https://github.com/E1Bos/VALocker/releases/latest"
+API_RELEASE_URL = "https://api.github.com/repos/E1Bos/VALocker/releases/latest"
 TEMPLATE_FOLDER_NAME = "app_defaults"
