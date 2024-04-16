72db68a47c7c39221c10b8bff0b5f5b0effc259e
E1Bos <lboscolo68@gmail.com>
2024-04-16 18:11:44 +0200

    Adjusted spacing

--- src/CustomLogger.py
+++ src/CustomLogger.py
@@ -2,6 +2,7 @@ import logging
 import os
 from ProjectUtils import FOLDER, GET_WORKING_DIR
 
+
 class CustomLogger:
     """
     A custom logger class for logging messages to a file.
@@ -16,26 +17,25 @@ class CustomLogger:
 
     """
 
-    def __init__(self, name: str, log_file: str="VALocker.log"):
+    def __init__(self, name: str, log_file: str = "VALocker.log"):
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.INFO)
         formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
         )
-        
+
         working_dir = GET_WORKING_DIR()
         log_path = os.path.join(working_dir, FOLDER.LOGS.value, log_file)
-        
+
         # Create the log directory if it doesn't exist
         os.makedirs(os.path.dirname(log_path), exist_ok=True)
-        
+
         # File handler
-        file_handler = logging.FileHandler(
-            log_path, mode="a"
-        )
+        file_handler = logging.FileHandler(log_path, mode="a")
         file_handler.setFormatter(formatter)
         self.logger.addHandler(file_handler)
-        
+
         # Stream handler
         stream_handler = logging.StreamHandler()
         stream_handler.setFormatter(formatter)
