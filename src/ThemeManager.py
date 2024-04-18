8a41dabbbf41f8acafcd33857028ee6a5b224637
E1Bos <lboscolo68@gmail.com>
2024-04-18 14:56:35 +0200

    Added ThemeManager for managing QT style settings

---
+++ src/ThemeManager.py
@@ -0,0 +1,72 @@
+import json
+import colorsys
+
+
+class ThemeManager:
+    def __init__(self) -> None:
+        self.theme = None
+
+        with open("style.qss", "r") as style_file:
+            self.style_sheet = style_file.read()
+
+    def setTheme(self, theme_name: str) -> None:
+        """
+        Sets the theme for the application, and brightens the colors for hover effects.
+
+        Args:
+            theme_name (str): The name of the theme to set.
+        """
+        with open(f"themes/{theme_name}-theme.json", "r") as theme_file:
+            self.theme = json.load(theme_file)
+
+        for element_to_brighten in [
+            "@accent",
+            "@button-enabled",
+            "@button-disabled",
+            "@foreground-highlight",
+        ]:
+            self.theme[f"@{element_to_brighten}-hover"] = self.brightenColor(
+                self.theme[element_to_brighten], 1.1
+            )
+
+        for key, value in self.theme.items():
+            self.style_sheet = self.style_sheet.replace(f"{key};", f"{value};")
+
+        
+
+    def brightenColor(self, hex_color: str, amount: int):
+        """
+        Brightens a given hex color by increasing its lightness.
+
+        Args:
+            hex_color (str): The hex color code to be brightened.
+            amount (int): The amount by which to increase the lightness of the color.
+
+        Returns:
+            str: The brightened hex color code.
+
+        """
+        hex_color = hex_color.lstrip("#")
+
+        rgb_color = tuple(int(hex_color[i : i + 2], 16) for i in (0, 2, 4))
+
+        h, l, s = colorsys.rgb_to_hls(
+            rgb_color[0] / 255.0, rgb_color[1] / 255.0, rgb_color[2] / 255.0
+        )
+
+        # Increase the lightness
+        l = max(min(l * amount, 1), 0)
+
+        r, g, b = colorsys.hls_to_rgb(h, l, s)
+
+        # Convert RGB back to hex
+        return "#{:02x}{:02x}{:02x}".format(int(r * 255), int(g * 255), int(b * 255))
+
+    def getTheme(self):
+        if self.theme:
+            return self.style_sheet
+
+
+if __name__ == "__main__":
+    theme_manager = ThemeManager()
+    theme_manager.setTheme("default")
