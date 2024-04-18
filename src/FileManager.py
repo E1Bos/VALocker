c094294aa694895ad5d9024f1e5f2ed15349cf6e
E1Bos <lboscolo68@gmail.com>
2024-04-18 16:22:01 +0200

    Updated migrating functions

--- src/FileManager.py
+++ src/FileManager.py
@@ -16,10 +16,10 @@ class FileManager:
 
     Methods:
         setup_file_manager(): Sets up the FileManager by ensuring that the required files exist and reading them into memory.
-        update_file(constants.Files): Update a file by downloading the latest version from the repository.
-        get_config(constants.Files): Returns the configuration dictionary for the specified file.
-        set_config(constants.Files, dict): Sets the configuration dictionary for the specified file.
-        """
+        update_file(FILE): Update a file by downloading the latest version from the repository.
+        get_config(FILE): Returns the configuration dictionary for the specified file.
+        set_config(FILE, dict): Sets the configuration dictionary for the specified file.
+    """
 
     # TODO: Replace "app_defaults/" with None when the app is ready for release
     def __init__(self) -> None:
@@ -36,14 +36,13 @@ class FileManager:
             FILE.STATS,
             FILE.LOCKING_INFO,
             FILE.AGENT_CONFIG,
-            FILE.USER_SETTINGS,
             FILE.SETTINGS,
             FILE.DEFAULT_SAVE,
             FILE.DEFAULT_THEME,
         }
 
         # Main directory for the files
-        self._MAIN_DIR = GET_WORKING_DIR()
+        self._WORKING_DIR = GET_WORKING_DIR()
 
         # URL to download the files from
         self._DOWNLOAD_URL = f"{URL.DOWNLOAD_URL.value}/{FOLDER.DEFAULTS.value}/"
@@ -74,7 +73,7 @@ class FileManager:
         """
 
         # Creates appdata/roaming/VALocker if it doesn't exist
-        os.makedirs(self._MAIN_DIR, exist_ok=True)
+        os.makedirs(self._WORKING_DIR, exist_ok=True)
 
         # Creates the required directories if they don't exist
         for folder in self._REQUIRED_FOLDERS:
@@ -85,7 +84,9 @@ class FileManager:
             file_path = self._absolute_file_path(file.value)
             if not os.path.isfile(file_path):
                 self._logger.info(f"{file_path} not found, downloading")
-                self._download_file(file.value, file_path)
+                file_data = self._download_file(file)
+                with open(file_path, "w") as f:
+                    json.dump(file_data, f, indent=4)
 
         settings_rel_path = FILE.SETTINGS.value
         self._settings = json.load(
@@ -96,36 +97,37 @@ class FileManager:
             self._logger.info("Files may need to be migrated, checking for old files")
             self._migrate_old_files()
 
-    def _download_file(self, file_path: str, save_path: str) -> None:
+    def _download_file(self, file: FILE) -> dict:
         """
-        Download a file from a public GitHub repository.
+        Download a file from a public GitHub repository and returns the content.
 
         Args:
-            file_path (str): The path of the file in the repository.
-            save_path (str): The path where the file should be saved.
+            file (FILE): The enum of the file in the repository.
+
+        Returns:
+            dict: The content of the downloaded file.
 
         Raises:
             requests.exceptions.RequestException: If the request to download the file fails.
         """
-        url = f"{self._DOWNLOAD_URL}{file_path}"
+        url = f"{self._DOWNLOAD_URL}{file.value}"
 
-        self._logger.info(f"Downloading {file_path} from {url}")
+        self._logger.info(f"Downloading {file.value} from {url}")
 
         try:
             response = requests.get(url)
             response.raise_for_status()  # Raise an exception if the request failed
-            with open(save_path, "wb") as f:
-                f.write(response.content)
-            self._logger.info(f"Downloaded {file_path}")
+            self._logger.info(f"Downloaded {file.value}")
+            return json.loads(response.text)
         except requests.exceptions.RequestException as e:
-            self._logger.error(f"Error downloading {file_path}: {e}")
+            self._logger.error(f"Error downloading {file.value}: {e}")
             raise e
 
     # endregion
 
     # region: Migrating old files
 
-    def _migrate_old_files(self):
+    def _migrate_old_files(self) -> None:
         """
         Check if the old VALocker directory exists and move the files to the new directory.
 
@@ -135,6 +137,7 @@ class FileManager:
         - Migrates the old `user_settings.json` file to the new directory.
         - Migrates the files in the `save_files` directory to the new directory.
         - Updates the settings to indicate that the migration has already been performed.
+        - Deletes `stats.json`, `user_settings.json`, and the `save_files` directory.
 
         Note: This method assumes that the new directory structure has already been set up.
         """
@@ -153,11 +156,20 @@ class FileManager:
         if os.path.exists(stats_file):
             self._logger.info("Found old stats.json file, migrating to new directory")
 
-            # Migrate the old stats.json file to the new directory
-            self._override_json_file(stats_file, FILE.STATS.value, delete_old=False)
-            self._override_json_file(
-                stats_file, FILE.USER_SETTINGS.value, delete_old=False
-            )
+            stats_data = json.load(open(stats_file, "r"))
+
+            # Replace old keys with new names
+            stats_data["TTL"] = stats_data.get("TIME_TO_LOCK")[-1]
+            stats_data["TTL_SAFE"] = [
+                stats_data["TIME_TO_LOCK"][safe_strength] for safe_strength in range(3)
+            ]
+
+            # Migrate the old stats data to the new file structure
+            self._migrate_json_data(stats_data, FILE.STATS)
+            self._migrate_json_data(stats_data, FILE.SETTINGS)
+
+            os.remove(stats_file)
+            self._logger.info(f"Deleted {stats_file}")
 
         # Check for user_settings file
         user_settings_file = os.path.join(old_dir, "user_settings.json")
@@ -165,9 +177,15 @@ class FileManager:
             self._logger.info(
                 "Found old user_settings.json file, migrating to new directory"
             )
-            self._override_json_file(
-                user_settings_file, FILE.SETTINGS.value, delete_old=False
+            user_settings_data = json.load(open(user_settings_file, "r"))
+
+            user_settings_data["ACTIVE_SAVE_FILE"] = (
+                f'{user_settings_data["ACTIVE_SAVE_FILE"]}.json'
             )
+            self._migrate_json_data(user_settings_data, FILE.SETTINGS)
+
+            os.remove(user_settings_file)
+            self._logger.info(f"Deleted {user_settings_file}")
 
         # Check for save_files directory
         save_files_dir = os.path.join(old_dir, "save_files")
@@ -175,20 +193,80 @@ class FileManager:
             self._logger.info(
                 "Found old save_files directory, migrating to new directory"
             )
-            # Iterate over the files in the save_files directory and move them to the new directory
+
             for file_name in os.listdir(save_files_dir):
-                save_file_location = os.path.join(save_files_dir, file_name)
-                new_save_file_location = os.path.join(
-                    self._MAIN_DIR, "save_files", file_name
-                )
-                shutil.move(save_file_location, new_save_file_location)
-                self._logger.info(f"Migrated {file_name} to {new_save_file_location}")
-            # Remove the old save_files directory
+                full_path = os.path.join(save_files_dir, file_name)
+                self._migrate_old_save_file(full_path, file_name)
+
+            self._logger.info(f"Migrated all save files in {save_files_dir}")
+
+            # Remove the old save_files dir
             os.rmdir(save_files_dir)
             self._logger.info(f"Deleted {save_files_dir}")
 
         self._logger.info("Migration complete")
         self._set_migrated_flag(True)
+
+    def _migrate_json_data(
+        self, data_to_migrate: dict, file_to_migrate_to: FILE
+    ) -> None:
+        """
+        Migrates a JSON file to a new directory.
+
+        Args:
+            data_to_migrate (dict): The data of the JSON file to migrate.
+            file_to_migrate_to (FILE): The file enum to migrate the JSON file to.
+
+        Returns:
+            None
+        """
+
+        new_file = json.load(
+            open(self._absolute_file_path(file_to_migrate_to.value), "r")
+        )
+        for key in data_to_migrate:
+            if key in new_file:
+                new_file[key] = data_to_migrate[key]
+
+        with open(self._absolute_file_path(file_to_migrate_to.value), "w") as f:
+            json.dump(new_file, f, indent=4)
+
+        self._logger.info(
+            f"Migrated old values to {self._absolute_file_path(file_to_migrate_to.value)}"
+        )
+
+    def _migrate_old_save_file(
+        self, old_save_file_path: str, old_save_name: str
+    ) -> None:
+
+        save_data = json.load(open(old_save_file_path, "r"))
+
+        current_active_agent = save_data.get("SELECTED_AGENT")
+
+        agent_status_list = save_data.get("UNLOCKED_AGENTS")
+        agent_random_list = save_data.get("RANDOM_AGENTS")
+
+        agents = dict()
+
+        for agent in agent_status_list.keys():
+            agents[agent] = [agent_status_list[agent], agent_random_list[agent]]
+
+        new_save_data = {
+            "SELECTED_AGENT": current_active_agent,
+            "AGENTS": agents,
+            "MAP_SPECIFIC_AGENTS": save_data.get("MAP_SPECIFIC_AGENTS"),
+        }
+
+        new_save_file_location = os.path.join(
+            self._WORKING_DIR, FOLDER.SAVE_FILES.value, old_save_name
+        )
+
+        with open(new_save_file_location, "w") as f:
+            json.dump(new_save_data, f, indent=4)
+
+        os.remove(old_save_file_path)
+
+        self._logger.info(f"Migrated {old_save_name} to {new_save_file_location}")
 
     def _set_migrated_flag(self, value: bool) -> None:
         """
@@ -197,50 +275,9 @@ class FileManager:
         Args:
             value (bool): The value to set for the 'ALREADY_MIGRATED' flag.
 
-        Returns:
-            None
         """
         self._settings["ALREADY_MIGRATED"] = value
         self.set_config(FILE.SETTINGS, self._settings)
-
-    def _override_json_file(
-        self, old_file_path, new_file_path, delete_old=False
-    ) -> None:
-        """
-        Overrides the contents of a JSON file with the contents of another JSON file, only where keys match.
-
-        Args:
-            old_file_path (str): The path to the old JSON file.
-            new_file_path (str): The path to the new JSON file.
-            delete_old (bool, optional): Whether to delete the old file after overriding. Defaults to False.
-
-        Returns:
-            None
-        """
-        old_stats_json = json.load(open(old_file_path, "r"))
-        new_stats_json = json.load(open(self._absolute_file_path(new_file_path), "r"))
-
-        # Update the new stats.json file
-        for key, value in old_stats_json.items():
-            if key in new_stats_json:
-                new_stats_json[key] = value
-
-        # Save the updated new_stats_json
-        with open(self._absolute_file_path(new_file_path), "w") as f:
-            json.dump(new_stats_json, f, indent=4)
-
-        self._logger.info(
-            f"Migrated {old_file_path} to {self._absolute_file_path(new_file_path)}"
-        )
-
-        # Delete the old file
-        if delete_old:
-            os.remove(old_file_path)
-            self._logger.info(f"Deleted {old_file_path}")
-
-    def _migrate_old_save_files(self):
-        # TODO: Impletment migration in format agent: [unlocked, random]
-        pass
 
     # endregion
 
@@ -264,7 +301,7 @@ class FileManager:
         Returns:
             str: The absolute file path.
         """
-        return os.path.join(self._MAIN_DIR, *args)
+        return os.path.join(self._WORKING_DIR, *args)
 
     def get_config(self, file: FILE) -> dict:
         """
@@ -326,7 +363,7 @@ class FileManager:
         # TODO: Make this method not override settings already present
 
         Args:
-            file_path (constants.Files): The file enum that needs to be updated.
+            file_path (FILE): The file enum that needs to be updated.
         """
         self._logger.info(f"Updating {file.value} to the latest version")
 
@@ -339,25 +376,6 @@ class FileManager:
         self._logger.info(f"Updated {file.value} to the latest version")
 
     # endregion
-
-    def get_theme(self, theme_name: str) -> dict:
-        """
-        Load a theme from the themes directory.
-
-        Args:
-            theme_name (str): The name of the theme to load.
-
-        Returns:
-            dict: The theme configuration dictionary.
-        """
-
-        if theme_name.rfind("-theme.json") == -1:
-            theme_name += "-theme.json"
-
-        theme_path = os.path.join(
-            self._absolute_file_path(FOLDER.THEMES.value, theme_name)
-        )
-        return json.load(open(theme_path, "r"))
 
 
 if __name__ == "__main__":
