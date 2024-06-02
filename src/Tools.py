28092807d59a9e708a5e2a0eabd13d5246755b7c
E1Bos <lboscolo68@gmail.com>
2024-06-02 19:57:14 +0100

    Added proper anti afk functionality, and improved tools thread logic (similar to instalocker thread) to ensure that the thread is running when it says its running and isnt spawning multiple threads.

--- src/Tools.py
+++ src/Tools.py
@@ -1,6 +1,7 @@
 import time
 import threading
 from enum import Enum, auto
+import random
 
 from typing import TYPE_CHECKING, Optional
 
@@ -8,6 +9,8 @@ if TYPE_CHECKING:
     from VALocker import VALocker
 
 from customtkinter import BooleanVar, IntVar
+# import pynput.mouse as pynmouse
+import pynput.keyboard as pynkeyboard
 
 # Custom Imports
 from CustomLogger import CustomLogger
@@ -23,74 +26,162 @@ class Tools():
     
     stop_flag = False
     
+    tools_input: bool = False
+    
+    keybinds = {
+            "MoveForward": "w",
+            "MoveBackward": "s",
+            "MoveRight": "d",
+            "MoveLeft": "a",
+        }
+    
+    movement_type: ANTI_AFK = ANTI_AFK.RANDOM_CENTERED
     anti_afk: BooleanVar
     drop_spike = BooleanVar
     
+    keyboard: pynkeyboard.Controller = pynkeyboard.Controller()
+    keyboard_listener: pynkeyboard.Listener = None
+    
     # Thread
     thread: Optional[threading.Thread] = None
+    stop_event: threading.Event
+    
     
     def __init__(self, parent:"VALocker"):
         self.anti_afk = parent.anti_afk
-        self.drop_spike = parent.drop_spike
+        self.keyboard_listener = None
+        self.stop_event = threading.Event()
+        
+    def keyboard_on_press(self, key) -> None:
+        if (
+            hasattr(key, "char")
+            and not self.tools_input
+            and not self.stop_flag
+            and self.anti_afk.get()
+        ):
+            self.anti_afk.set(False)
     
     # region: Threading
 
-    def main(self):
-        while not self.stop_flag:
-            time.sleep(1)
+    def main(self) -> None:
+        self.keyboard_listener = pynkeyboard.Listener(
+            on_press=self.keyboard_on_press
+        )
+        self.keyboard_listener.start()
+        
+        self.logger.info("Thread started")
+        
+        before = 0
+        while not self.stopped():
+            now = time.time()
             
             if self.anti_afk.get():
-                self.anti_afk_main()
+                if now - before >= 7.5:
+                    self.anti_afk_main(self.movement_type)
+                    before = now
             
-            if self.drop_spike.get():
-                self.drop_spike_main()
-            # TODO: Implement logic
+            self.stop_event.wait(3)
+        
+        self.logger.info("Thread stopped")
+        self.keyboard_listener.stop()        
 
-    def anti_afk_main(self):
-        movement_type = ANTI_AFK.RANDOM
+    def anti_afk_main(self, movement_type: ANTI_AFK = ANTI_AFK.RANDOM_CENTERED) -> None:
+        
+        queue = []
         
         match movement_type:
-            case ANTI_AFK.RANDOM:
-                self.logger.info("Random Movement")
             case ANTI_AFK.RANDOM_CENTERED:
-                self.logger.info("Random Centered Movement")
+                # Random key presses but end up back in the center
+                direction = random.choice(
+                    ["forward", "right", "backward", "left"]
+                )
+                match direction:
+                    case "forward":
+                        keybind_order = [
+                            self.keybinds["MoveForward"],
+                            self.keybinds["MoveBackward"],
+                        ]
+                    case "backward":
+                        keybind_order = [
+                            self.keybinds["MoveBackward"],
+                            self.keybinds["MoveForward"],
+                        ]
+                    case "right":
+                        keybind_order = [
+                            self.keybinds["MoveRight"],
+                            self.keybinds["MoveLeft"],
+                        ]
+                    case "left":
+                        keybind_order = [
+                            self.keybinds["MoveLeft"],
+                            self.keybinds["MoveRight"],
+                        ]
+                
+            case ANTI_AFK.RANDOM:
+                # Random key presses
+                possible_directions = [
+                    self.keybinds["MoveForward"],
+                    self.keybinds["MoveRight"],
+                    self.keybinds["MoveBackward"],
+                    self.keybinds["MoveLeft"],
+                ]
+                keybind_order = [random.choice(possible_directions),]
+                
             case ANTI_AFK.CIRCLE:
-                self.logger.info("Circle Movement")
+                # Go in a circle
+                keybind_order = [
+                    self.keybinds["MoveForward"],
+                    self.keybinds["MoveRight"],
+                    self.keybinds["MoveBackward"],
+                    self.keybinds["MoveLeft"],
+                ]
+                    
             case ANTI_AFK.STRAFE:
-                self.logger.info("Strafe Movement")
+                # Go right to left
+                keybind_order = [
+                    self.keybinds["MoveRight"],
+                    self.keybinds["MoveLeft"],
+                ]
             case _:
                 self.logger.error(f"Unrecognized Movement Type {movement_type}")
+                self.anti_afk_main(ANTI_AFK.RANDOM_CENTERED)
 
-        raise NotImplementedError("Anti-AFK not implemented")
+        queue.extend(keybind_order)
 
-    def drop_spike_main(self):
-        raise NotImplementedError("Drop Spike not implemented")
+        for key in queue:
+            self.tools_input = True
+            
+            self.keyboard.press(key)
+            self.stop_event.wait(random.uniform(0.1, 0.3))
+            
+            self.keyboard.release(key)
+            self.tools_input = False
+            self.stop_event.wait(random.uniform(0.1, 0.3))
 
-    def start(self):
+    def start(self) -> None:
         # Check if thread is already running, if so return
         if self.thread is not None and self.thread.is_alive():
             self.logger.warning("Thread already running")
-            return
 
         # Start thread
-        self.stop_flag = False
-        self.logger.info("Thread started")
+        self.stop_event.clear()
         self.thread = threading.Thread(target=self.main, daemon=True)
         self.thread.start()
 
-    def stop(self):
+    def stop(self) -> None:
         # Ensure thread exists
         if self.thread is None:
             self.logger.info("Thread not started")
             return
         
-        # If thread is not alive, thread is already stopped
-        if not self.thread.is_alive():
-            self.logger.warning("Thread already stopped")
+        if threading.current_thread() is self.thread:
+            self.logger.info("Cannot stop from within the thread itself")
             return
 
         # Stop thread
-        self.stop_flag = True
-        self.logger.info("Thread stopped")
+        self.stop_event.set()
+        
+    def stopped(self):
+        return self.stop_event.is_set()
 
     # endregion
