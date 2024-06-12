e0f0385aa2afd85aef20a485f059bee0e727dd30
E1Bos <lboscolo68@gmail.com>
2024-06-12 18:31:26 +0200

    Fixed imports in ValockerApp

--- VALockerApp.py
+++ VALockerApp.py
@@ -1,5 +1,9 @@
 import sys
-sys.path.append("src")
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+src_path = os.path.join(current_dir, 'src')
+sys.path.append(src_path)
 
 from VALocker import VALocker
 
