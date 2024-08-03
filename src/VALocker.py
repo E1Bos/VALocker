81a7286398c567ff02b50ed94b97136d7ab0ad42
E1Bos <lboscolo68@gmail.com>
2024-08-03 23:39:37 +0200

    Updated to work with new VALORANT UI

--- src/VALocker.py
+++ src/VALocker.py
@@ -4,12 +4,12 @@ from typing import Optional
 from sys import exit as sys_exit
 
 from webbrowser import open as web_open
-from threading import Lock, Thread
+from threading import Lock
 from ctypes import windll
 
 # Custom imports
 from CustomLogger import CustomLogger
-from Constants import FILE, FOLDER, FRAME, ICON, LOCKING_CONFIG, ANTI_AFK
+from Constants import FILE, FOLDER, FRAME, ICON, LOCKING_CONFIG, ANTI_AFK, ROLE, AgentIndex
 from FileManager import FileManager
 from SaveManager import SaveManager
 from Updater import Updater
@@ -67,7 +67,7 @@ class VALocker(CTk):
     temp_random_agents: dict[str, bool]
     exclusiselect: BooleanVar
     map_specific: BooleanVar
-    agent_index: IntVar
+    agent_index: AgentIndex
 
     # Tools Variables
     num_running_tools: int = 0
@@ -137,9 +137,17 @@ class VALocker(CTk):
 
         self.instalocker_status = BooleanVar(value=True)
 
-        self.fast_mode_timings = self.file_manager.get_value(
+        fast_mode_timings = self.file_manager.get_value(
             FILE.SETTINGS, "fastModeTimings"
         )
+        self.fast_mode_timings = [
+            (
+                fast_mode_timings.get("clickDelay", 0.02)
+                if i % 2 == 0
+                else fast_mode_timings.get("moveDelay", 0.02)
+            )
+            for i in range(5)
+        ]
 
         self.safe_mode_enabled = BooleanVar(
             value=self.file_manager.get_value(FILE.SETTINGS, "safeModeOnStartup", False)
@@ -192,7 +200,7 @@ class VALocker(CTk):
         self.temp_random_agents = None
         self.exclusiselect = BooleanVar(value=False)
         self.map_specific = BooleanVar(value=False)
-        self.agent_index = IntVar(value=0)
+        self.agent_index = AgentIndex(ROLE.CONTROLLER, 0)
 
         # Tools Vars
         self.pause_tools_thread = BooleanVar(value=False)
@@ -338,9 +346,9 @@ class VALocker(CTk):
 
         for item in self.updater.ITEMS_TO_CHECK:
             if type(item) is FOLDER:
-                
+
                 files = self.file_manager.get_files_in_folder(item)
-                
+
                 for config_file in files:
                     try:
                         config_enum = LOCKING_CONFIG(config_file)
@@ -691,15 +699,37 @@ class VALocker(CTk):
     # endregion
 
     # region: Locking Functions
+    def get_agent_role_and_index(self, agent: str) -> tuple[ROLE, int]:
+        """
+        Returns the role and index of the given agent.
+
+        Args:
+            agent (str): The agent to get the role and index of.
+
+        Returns:
+            tuple[ROLE, int]: The role and index of the agent.
+        """
+        for role in self.file_manager.get_value(FILE.AGENT_CONFIG, "roles"):
+            agents = self.file_manager.get_value(FILE.AGENT_CONFIG, role)
+
+            if agent in agents:
+                unlocked_role_agents = [
+                    agent for agent in agents if agent in self.get_unlocked_agents()
+                ]
+
+                
+                return ROLE(role), unlocked_role_agents.index(agent)
+
+        return None
+
     def set_locking_agent(self, *_) -> None:
         """
         Sets the agent index to the selected agent.
         """
-        unlocked_agents = self.get_unlocked_agents()
+        role, index = self.get_agent_role_and_index(self.selected_agent.get().lower())
 
-        index = unlocked_agents.index(self.selected_agent.get().lower())
-        self.agent_index.set(index)
-
+        self.agent_index.set_agent(role, index)
+        
     def get_unlocked_agents(self) -> list[str]:
         """
         Returns a list of all unlocked agents, sorted alphabetically.
